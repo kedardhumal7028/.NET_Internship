2335493d1235cabe9f482118368fbf0a39f144aa
kedardhumal7028 <kedardhumal31@gmail.com>
2022-03-16 23:11:58 +0530

    Web Design

---
+++ script.js
@@ -0,0 +1,9 @@
+function getdata(){
+    const Name = document.getElementById("name").value;
+    const Email = document.getElementById("email").value;
+    const College = document.getElementById("clg").value;
+    alert(`Name : ${Name}
+ Email : ${Email}
+Collage : ${College}
+    `);
+}
